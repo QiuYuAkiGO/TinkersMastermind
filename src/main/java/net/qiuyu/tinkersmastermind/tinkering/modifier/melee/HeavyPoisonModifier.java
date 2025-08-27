2d42fb4e7ee6adbde5f7d65c17f6aac191947a14
Diacletia <sizhelius.li@mail.utoronto.ca>
2025-08-27 22:28:50 +0800

    0.0.5b-修复了曼陀罗合金使玛玉灵增伤失效的bug，并优化了中毒逻辑

--- src/main/java/net/qiuyu/tinkersmastermind/tinkering/modifier/melee/HeavyPoisonModifier.java
+++ src/main/java/net/qiuyu/tinkersmastermind/tinkering/modifier/melee/HeavyPoisonModifier.java
@@ -23,22 +23,30 @@ public class HeavyPoisonModifier extends Modifier implements MeleeHitModifierHoo
 
     @Override
     public void afterMeleeHit(IToolStackView tool, ModifierEntry modifier, ToolAttackContext context, float damageDealt) {
-        // 根据工具等级赋予对方1/2级的中毒效果
+        // 根据工具等级赋予对方中毒效果，等级越高每次增加时间越长。
         int level = modifier.getLevel();
         LivingEntity target = context.getLivingTarget();
         if (target != null && !target.isDeadOrDying() && context.isFullyCharged()){
-            context.getLivingTarget().addEffect(new MobEffectInstance(MobEffects.POISON, 100, level - 1));
+            MobEffectInstance existingPosion = target.getEffect(MobEffects.POISON);
+            int newDuration = 100;
+            int newAmplifier = level-1;
+            if(existingPosion!=null){
+                newDuration = Math.min(existingPosion.getDuration()+level*25,2400);
+                newAmplifier = Math.max(existingPosion.getAmplifier(),newAmplifier);
+            }
+            context.getLivingTarget().addEffect(new MobEffectInstance(MobEffects.POISON, newDuration, newAmplifier));
         }
     }
 
     @Override
-    public float getMeleeDamage(IToolStackView view, ModifierEntry entry, ToolAttackContext context, float v, float v1) {
-        //  如果攻击时身上有中毒效果,则根据中毒剩余时间增加造成的伤害,最高20％  每3秒1％(也就是一分钟为最大值).
-        float amplifier = 1F;
-        LivingEntity attacker = context.getAttacker();
-        if (attacker.hasEffect(MobEffects.POISON)){
-           amplifier += Math.min((float)attacker.getEffect(MobEffects.POISON).getDuration() * 0.2F / 1200 ,0.2F);
+    public float getMeleeDamage(IToolStackView view, ModifierEntry entry, ToolAttackContext context, float baseDamage, float damage) {
+        //  如果攻击时目标有中毒效果,则根据中毒剩余时间增加造成的伤害,最高20％  每3秒1％(也就是一分钟为最大值).
+        LivingEntity target = context.getLivingTarget();
+        if (target.hasEffect(MobEffects.POISON)){
+           float amplifier = 1F;
+           amplifier += Math.min((float)target.getEffect(MobEffects.POISON).getDuration() * 0.2F / 1200 ,0.2F);
+           return damage * amplifier;
         }
-        return v * amplifier;
+        return damage;
     }
 }
