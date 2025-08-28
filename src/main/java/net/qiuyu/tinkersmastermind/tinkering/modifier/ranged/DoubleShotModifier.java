14f47eaecfef4e4dbc93bf298a0bafa80a68fd56
QiuYuAkiGO <qiuyuakigo@gmail.com>
2025-08-28 15:36:20 +0800

    1.尝试修复双头锥刺不能触发伤害的问题; 2.修复了流体没有显示名字的问题(通过jade模组发现)

--- src/main/java/net/qiuyu/tinkersmastermind/tinkering/modifier/ranged/DoubleShotModifier.java
+++ src/main/java/net/qiuyu/tinkersmastermind/tinkering/modifier/ranged/DoubleShotModifier.java
@@ -40,6 +40,7 @@ public class DoubleShotModifier extends Modifier implements ProjectileHitModifie
                 && target.isAlive() && projectile instanceof AbstractArrow arrow){
             float damageDealt = (float) ((float) arrow.getBaseDamage() * arrow.getDeltaMovement().length());
             target.playSound(SoundEvents.GLASS_BREAK);
+            target.invulnerableTime = 0;
             target.hurt(
                     new DamageSource(attacker.getCommandSenderWorld().registryAccess().registryOrThrow(Registries.DAMAGE_TYPE).getHolderOrThrow(DamageTypes.PLAYER_ATTACK)),
                     damageDealt);
